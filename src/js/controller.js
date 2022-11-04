c6b045c79ecad72a07cd41c0ef215afa50484991
pri-yanshus <sharmapriyanshu504@gmail.com>
2022-11-04 12:51:11 +0530

    Added new Feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,6 +118,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('welcome');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -126,5 +130,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
